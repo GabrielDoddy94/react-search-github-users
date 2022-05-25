ae3bf8b4f4fef19caa33e2a182be246525e52474
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-05-25 16:45:11 -0300

    error page

--- src/pages/Error/index.tsx
+++ src/pages/Error/index.tsx
@@ -1,5 +1,17 @@
 import { Link } from "react-router-dom";
 
+import { Wrapper } from "./styles";
+
 export function Error() {
-  return <h2>error page</h2>;
+  return (
+    <Wrapper>
+      <div>
+        <h1>404</h1>
+        <h3>sorry, the page you tried cannot be found</h3>
+        <Link to="/" className="btn">
+          back home
+        </Link>
+      </div>
+    </Wrapper>
+  );
 }
